e183b72877de2936006e6b84af32ed5b3bfc9818
Michal Kolodziej <m.kolodziej2211@gmail.com>
2025-06-14 13:23:52 +0200

    initial commit

---
+++ src/main/java/com/example/weekweather/WeekweatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.weekweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekweatherApplication.class, args);
+	}
+
+}
