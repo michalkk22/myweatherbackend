91ae77c8c8e1d008b8e21823beb3ee95cc14156b
Michal Kolodziej <m.kolodziej2211@gmail.com>
2025-06-14 19:44:33 +0200

    refine forecast client

--- src/main/java/com/example/client/ForecastClient.java
+++ src/main/java/com/example/client/ForecastClient.java
@@ -1,23 +1,33 @@
 package com.example.client;
 
-import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.config.WeatherApiProperties;
 import com.example.model.ForecastApiModel;
 
+@Component
 public class ForecastClient {
-    private final RestTemplate restTemplate;
-    private final WeatherApiProperties properties;
+    private final WebClient webClient;
+    // Uncomment if there are more properties than URL
+    // private final WeatherApiProperties properties;
 
-    public ForecastClient(RestTemplate restTemplate, WeatherApiProperties weatherApiProperties) {
-        this.restTemplate = restTemplate;
-        this.properties = weatherApiProperties;
+    public ForecastClient(WebClient.Builder builder, WeatherApiProperties properties) {
+        this.webClient = builder.baseUrl(properties.getUrl()).build();
     }
 
     public ForecastApiModel getWeekForecast(double latitude, double longitude) {
-        String url = String.format("%s?latitude=%.2f&longitude=%.2f&daily=temperature_2m_max,temperature_2m_min,weathercode,sunshine_duration&timezone=auto",
-                properties.getUrl(), latitude, longitude);
-        
-        return restTemplate.getForObject(url, ForecastApiModel.class);
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/v1/forecast")
+                        .queryParam("latitude", latitude)
+                        .queryParam("longitude", longitude)
+                        .queryParam("daily", "temperature_2m_max", "temperature_2m_min", "weathercode",
+                                "sunshine_duration")
+                        .queryParam("timezone", "auto")
+                        .build())
+                .retrieve()
+                .bodyToMono(ForecastApiModel.class)
+                .block();
     }
 }
