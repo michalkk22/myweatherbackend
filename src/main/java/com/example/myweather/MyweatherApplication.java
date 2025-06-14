6dd8429a53056c5febc5c1478d6bdaa369c03f12
Michal Kolodziej <m.kolodziej2211@gmail.com>
2025-06-14 19:47:44 +0200

    remove usused code due to the previous commit

--- src/main/java/com/example/myweather/MyweatherApplication.java
+++ src/main/java/com/example/myweather/MyweatherApplication.java
@@ -2,8 +2,6 @@ package com.example.myweather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MyweatherApplication {
@@ -11,10 +9,4 @@ public class MyweatherApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyweatherApplication.class, args);
 	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
 }
